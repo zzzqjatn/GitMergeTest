a352b9610c03740d5d7ab9fc0c812fdfa0f4f43b
zzzqjatn <zzzqjatn@naver.com>
2023-03-08 11:28:29 +0900

    [pbs] setup for merage

---
+++ gitMergeTest/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace gitMergeTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Ruler ruler = new Ruler(10);
+            ruler.Run();
+        }
+    }
+
+    public class Ruler
+    {
+        private const float ONE_INCH = 2.54f;
+        public int Centimeter { get; set; } = 0;
+        public float Inch
+        {
+            get { return Centimeter * ONE_INCH; }
+            private set { Centimeter = (int)(value / ONE_INCH); }
+        }
+
+        public Ruler(int cmValue) { Centimeter = cmValue; }
+
+        public void Run()
+        {
+            Console.WriteLine($"{this.Centimeter} cm는 {this.Inch} inch 입니다.");
+        }
+    }
+}
